7bf2d92b7a19955812175cab60a5453b8c07c38c
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-16 14:19:13 +0900

    :sparkles: :: database 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,37 @@ repositories {
 }
 
 dependencies {
+	// jpa
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+	// database
+	implementation("mysql:mysql-connector-java:8.0.32")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.mariadb.jdbc:mariadb-java-client-jre6:1.6.1")
+
+	// web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// validation
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.3")
+	implementation("io.kotest:kotest-extensions-spring:4.4.3")
+	testImplementation("io.mockk:mockk:1.12.0")
+
+	// kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// security
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.security:spring-security-test")
+
 }
 
 tasks.withType<KotlinCompile> {
