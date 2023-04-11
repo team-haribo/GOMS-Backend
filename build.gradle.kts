0385af8a996f818e43e130c4bd3728255cf48319
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-11 16:44:03 +0900

    :sparkles: :: jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.security:spring-security-test")
 
-	// auth
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// GAuth
 	implementation("com.github.GSM-MSG:GAuth-SDK-Java:v2.0.0")
 
 }
