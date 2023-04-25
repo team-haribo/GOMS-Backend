c911e9e98d7764261181e888a78deb9d44025b66
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-25 10:22:28 +0900

    :sparkles: :: 지각 랭킹 조회 추가

---
+++ src/main/kotlin/com/project/goms/domain/late/usecase/QueryLateRankUseCase.kt
@@ -0,0 +1,29 @@
+package com.project.goms.domain.late.usecase
+
+import com.project.goms.domain.late.common.exception.LateAccountNotFoundException
+import com.project.goms.domain.late.persistence.repository.LateRepository
+import com.project.goms.domain.late.presentation.data.dto.LateRankDto
+import com.project.goms.global.annotation.UseCaseWithReadOnlyTransaction
+
+@UseCaseWithReadOnlyTransaction
+class QueryLateRankUseCase(
+    private val lateRepository: LateRepository
+) {
+
+    fun execute(): List<LateRankDto> {
+        val lateRank = lateRepository.findTop5ByOrderByAccountDesc()
+
+        if (lateRank.isEmpty()) {
+            throw LateAccountNotFoundException()
+        }
+
+        return lateRank.map {
+            LateRankDto(
+                accountIdx = it.account.idx,
+                LateRankDto.StudentNum(it.account.grade, it.account.classNum, it.account.number),
+                profileUrl = it.account.profileUrl
+            )
+        }
+    }
+
+}
