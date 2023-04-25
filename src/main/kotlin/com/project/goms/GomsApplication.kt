a987caea5d302621fc4d0ed3fc1bb60f7c9298f7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-25 10:20:49 +0900

    :sparkles: :: EnableScheduling 어노테이션 추가

--- src/main/kotlin/com/project/goms/GomsApplication.kt
+++ src/main/kotlin/com/project/goms/GomsApplication.kt
@@ -3,7 +3,9 @@ package com.project.goms
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class GomsApplication
