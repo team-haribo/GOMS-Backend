ad1d3b2bd27a88ee2998588ebba687bed091c7fc
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-25 10:16:37 +0900

    :sparkles: :: 학번순으로 사용자 조회 로직 추가

---
+++ src/main/kotlin/com/project/goms/domain/account/usecase/QueryAllAccountUseCase.kt
@@ -0,0 +1,24 @@
+package com.project.goms.domain.account.usecase
+
+import com.project.goms.domain.account.persistence.repository.AccountRepository
+import com.project.goms.domain.account.presentation.data.dto.AccountDto
+import com.project.goms.global.annotation.UseCaseWithReadOnlyTransaction
+
+@UseCaseWithReadOnlyTransaction
+class QueryAllAccountUseCase(
+    private val accountRepository: AccountRepository
+) {
+
+    fun execute() =
+        accountRepository.findAllByOrderByGradeDescClassNumDescNumberDesc()
+            .map {
+                AccountDto(
+                    accountIdx = it.idx,
+                    studentNum = AccountDto.StudentNum(it.grade, it.classNum, it.number),
+                    profileUrl = it.profileUrl,
+                    authority = it.authority
+                )
+            }
+
+
+}
