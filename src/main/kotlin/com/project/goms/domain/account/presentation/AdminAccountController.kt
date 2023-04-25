0b6a243f75792a976096d563e464ca2448f9cffd
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-25 11:03:38 +0900

    :bug: :: startEndPoint admin으로 변경

--- src/main/kotlin/com/project/goms/domain/account/presentation/AdminAccountController.kt
+++ src/main/kotlin/com/project/goms/domain/account/presentation/AdminAccountController.kt
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/account")
+@RequestMapping("/api/v1/admin")
 class AdminAccountController(
     private val accountConverter: AccountConverter,
     private val grantAuthorityUseCase: GrantAuthorityUseCase,
