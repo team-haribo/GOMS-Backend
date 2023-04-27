632259f3123537704d14d222f25714347eb909c1
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-27 20:09:26 +0900

    :sparkles: :: 토큰 만료기한 jsonFormat 추가

--- src/main/kotlin/com/project/goms/domain/auth/presentation/data/response/TokenResponse.kt
+++ src/main/kotlin/com/project/goms/domain/auth/presentation/data/response/TokenResponse.kt
@@ -1,12 +1,15 @@
 package com.project.goms.domain.auth.presentation.data.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.project.goms.domain.account.presentation.data.enums.Authority
 import java.time.LocalDateTime
 
 data class TokenResponse(
     val accessToken: String,
     val refreshToken: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH-mm-ss")
     val accessTokenExp: LocalDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH-mm-ss")
     val refreshTokenExp: LocalDateTime,
     val authority: Authority
 )
