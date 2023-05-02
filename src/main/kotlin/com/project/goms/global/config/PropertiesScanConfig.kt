f9fd43d19d7ffc44670cc729963950afd6f09294
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-02 12:28:40 +0900

    :sparkles: :: propertiesScanConfig 추가

---
+++ src/main/kotlin/com/project/goms/global/config/PropertiesScanConfig.kt
@@ -0,0 +1,17 @@
+package com.project.goms.global.config
+
+import com.project.goms.global.gauth.property.GAuthProperties
+import com.project.goms.global.security.jwt.common.properties.JwtExpTimeProperties
+import com.project.goms.global.security.jwt.common.properties.JwtProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan(
+    basePackageClasses = [
+        JwtProperties::class,
+        JwtExpTimeProperties::class,
+        GAuthProperties::class
+    ]
+)
+class PropertiesScanConfig
