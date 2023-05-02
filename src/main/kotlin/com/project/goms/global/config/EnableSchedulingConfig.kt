5bb2aea3b082645f720b48bc0a8eda10b57bde63
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-02 12:28:25 +0900

    :sparkles: :: enableSchedulingConfig 추가

---
+++ src/main/kotlin/com/project/goms/global/config/EnableSchedulingConfig.kt
@@ -0,0 +1,8 @@
+package com.project.goms.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class EnableSchedulingConfig
