e7997a23288ed4e195f0ef9c1f00721a9007d177
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-05 17:43:53 +0900

    :recycle: :: entity로 패키지 이동

---
+++ src/main/kotlin/com/project/goms/domain/account/entity/Authority.kt
@@ -0,0 +1,10 @@
+package com.project.goms.domain.account.entity
+
+enum class Authority(
+    name: String
+){
+
+    ROLE_STUDENT("학생"),
+    ROLE_STUDENT_COUNCIL("학생회");
+
+}
--- src/main/kotlin/com/project/goms/domain/account/presentation/data/enums/Authority.kt
+++
@@ -1,7 +0,0 @@
-package com.project.goms.domain.account.presentation.data.enums
-
-enum class Authority {
-
-    ROLE_STUDENT, ROLE_ADMIN
-
-}
