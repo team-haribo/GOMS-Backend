73197aca545f90a374ff615a7ed7a45a4205553e
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-05 20:13:17 +0900

    :sparkles: :: 권한 주기 테스트 코드 추가

---
+++ src/test/kotlin/com/project/goms/domain/studentCouncil/GrantAuthorityUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.project.goms.domain.studentCouncil
+
+import com.project.goms.common.AnyValueObjectGenerator
+import com.project.goms.domain.account.entity.Account
+import com.project.goms.domain.account.entity.repository.AccountRepository
+import com.project.goms.domain.studentCouncil.usecase.dto.GrantAuthorityDto
+import com.project.goms.domain.account.entity.Authority
+import com.project.goms.domain.studentCouncil.usecase.GrantAuthorityUseCase
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.data.repository.findByIdOrNull
+import java.util.UUID
+
+class GrantAuthorityUseCaseTest: BehaviorSpec({
+    val accountRepository = mockk<AccountRepository>()
+    val grantAuthorityUseCase = GrantAuthorityUseCase(accountRepository)
+
+    Given("grantAuthorityDto가 주어질때") {
+        val accountIdx = UUID.randomUUID()
+        val grantAuthorityDto = GrantAuthorityDto(accountIdx = accountIdx, authority = Authority.ROLE_STUDENT_COUNCIL)
+        val account = AnyValueObjectGenerator.anyValueObject<Account>("idx" to accountIdx)
+
+        every { accountRepository.findByIdOrNull(account.idx) } returns account
+        every { accountRepository.save(any()) } returns account
+
+        When("권한 수정 요청을 하면") {
+            grantAuthorityUseCase.execute(grantAuthorityDto)
+
+            Then("권한이 주정이 되어야 한다.") {
+                verify(exactly = 1) { accountRepository.save(any()) }
+            }
+        }
+    }
+})
