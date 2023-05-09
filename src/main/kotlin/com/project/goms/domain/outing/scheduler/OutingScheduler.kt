224bcc6c48ba3057fce8f7c9fdbb62f9dd1ec5bd
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-09 11:22:15 +0900

    :sparkles: :: 외출자 삭제 스케듈러 추가

--- src/main/kotlin/com/project/goms/domain/outing/scheduler/OutingScheduler.kt
+++ src/main/kotlin/com/project/goms/domain/outing/scheduler/OutingScheduler.kt
@@ -1,6 +1,7 @@
 package com.project.goms.domain.outing.scheduler
 
 import com.project.goms.domain.late.usecase.SaveLateAccountUseCase
+import com.project.goms.domain.outing.usecase.DeleteOutingStudentsUseCase
 import com.project.goms.domain.outing.usecase.ReminderOutingUseCase
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Component
 @Component
 class OutingScheduler(
     private val reminderOutingUseCase: ReminderOutingUseCase,
-    private val saveRateStudentUseCase: SaveLateAccountUseCase
+    private val saveRateStudentUseCase: SaveLateAccountUseCase,
+    private val deleteOutingStudentsUseCase: DeleteOutingStudentsUseCase,
 ) {
 
-    @Scheduled(cron = "0 40 18 ? * 3")
+    @Scheduled(cron = "0 40 18 ? * 3") // 매주 수요일 6시 40분에 외출 여부 디스코드를 보낸다.
     fun outingTenMinuteAgoScheduler() = reminderOutingUseCase.execute()
 
-    @Scheduled(cron = "0 30 7 ? * 3")
+    @Scheduled(cron = "0 30 7 ? * 3") // 매주 수요일 7시 30분에 지각자를 저장한다.
     fun checkRateStudent() = saveRateStudentUseCase.execute()
+
+    @Scheduled(cron = "0 50 7 ? * 3") // 매주 수요일 7시 50분에 외출자를 삭제한다.
+    fun deleteOutingStudents() = deleteOutingStudentsUseCase.execute()
 
 }
