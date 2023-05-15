1ff138d09b2edde496b5570c292cd68adc6bc2c7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-15 12:08:15 +0900

    :sparkles: :: webhook url 환경변수 처리 추가

--- src/main/kotlin/com/project/goms/infrastructure/feign/client/DiscordFeignClient.kt
+++ src/main/kotlin/com/project/goms/infrastructure/feign/client/DiscordFeignClient.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
-@FeignClient(name = "discordFeignClient", url = "https://discord.com/api/webhooks/1099876986568048780/8aKRYTIiVgbOZWrXYqXM8e_EtXsNTKFXoRvnY8AuXnUc8cuGXPALKrlKLv0a_rsfClcF")
+@FeignClient(name = "discordFeignClient", url = "\${discord.webhookUrl}")
 interface DiscordFeignClient {
 
     @PostMapping(headers = ["Content-Type: application/x-www-form-urlencoded"])
