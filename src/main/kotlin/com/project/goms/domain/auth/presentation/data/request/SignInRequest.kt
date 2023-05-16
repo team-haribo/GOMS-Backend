0c872d86380fa91b10f48d3c6711f177b1b0370c
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-16 17:39:34 +0900

    :ambulance: :: notNull 필드 옵션 추가

--- src/main/kotlin/com/project/goms/domain/auth/presentation/data/request/SignInRequest.kt
+++ src/main/kotlin/com/project/goms/domain/auth/presentation/data/request/SignInRequest.kt
@@ -1,5 +1,8 @@
 package com.project.goms.domain.auth.presentation.data.request
 
+import org.jetbrains.annotations.NotNull
+
 data class SignInRequest(
+    @field:NotNull
     val code: String
 )
--- src/main/kotlin/com/project/goms/domain/studentCouncil/presentation/data/request/GrantAuthorityRequest.kt
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/presentation/data/request/GrantAuthorityRequest.kt
@@ -1,9 +1,12 @@
 package com.project.goms.domain.studentCouncil.presentation.data.request
 
 import com.project.goms.domain.account.entity.Authority
+import org.jetbrains.annotations.NotNull
 import java.util.UUID
 
 data class GrantAuthorityRequest(
+    @field:NotNull
     val accountIdx: UUID,
+    @field:NotNull
     val authority: Authority
 )
