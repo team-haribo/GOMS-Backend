960149c1ab328aacd409c535dc8c21b9b5a10bf9
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-22 22:50:14 +0900

    :fire: :: lateRepositoryCustom 클래스 삭제

--- src/main/kotlin/com/project/goms/domain/late/entity/repository/LateRepositoryCustom.kt
+++
@@ -1,9 +0,0 @@
-package com.project.goms.domain.late.entity.repository
-
-import com.project.goms.domain.late.usecase.dto.LateRankDto
-
-interface LateRepositoryCustom {
-
-    fun findTop5ByOrderByAccountDesc(): List<LateRankDto>
-
-}
