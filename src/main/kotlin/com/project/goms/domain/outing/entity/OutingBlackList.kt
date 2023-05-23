8c143026de082ccb3c389626a3b0d440fb89ccf5
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-23 10:02:23 +0900

    :sparkles: :: indexed 어노테이션 추가

--- src/main/kotlin/com/project/goms/domain/outing/entity/OutingBlackList.kt
+++ src/main/kotlin/com/project/goms/domain/outing/entity/OutingBlackList.kt
@@ -3,14 +3,17 @@ package com.project.goms.domain.outing.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 @RedisHash("outing_black_list")
 data class OutingBlackList(
     @Id
+    @Indexed
     val accountIdx: UUID,
 
+    @Indexed
     @TimeToLive(unit = TimeUnit.SECONDS)
     val expiredAt: Int
 )
