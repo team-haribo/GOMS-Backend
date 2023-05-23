d2465cf30fb03a6641255b1980f1653221dd49db
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-23 13:27:40 +0900

    :recycle: :: GAuthConfig로 클래스 네이밍 변경

--- src/main/kotlin/com/project/goms/global/gauth/GAuthConfig.kt
+++ src/main/kotlin/com/project/goms/global/gauth/GAuthConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class GauthConfiguration {
+class GAuthConfig {
 
     @Bean
     fun gauth(): GAuth = GAuthImpl()
