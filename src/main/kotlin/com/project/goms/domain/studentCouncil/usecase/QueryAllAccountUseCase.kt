aaa54a11b7c82993fadbe9e2f5e7bb11181ea38b
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-23 10:04:27 +0900

    :recycle: :: 전체 학생 조회 최적화 리펙토링

--- src/main/kotlin/com/project/goms/domain/studentCouncil/usecase/QueryAllAccountUseCase.kt
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/usecase/QueryAllAccountUseCase.kt
@@ -1,6 +1,6 @@
 package com.project.goms.domain.studentCouncil.usecase
 
-import com.project.goms.domain.account.entity.repository.AccountRepository
+import com.project.goms.domain.account.entity.repository.CustomAccountRepository
 import com.project.goms.domain.account.usecase.dto.StudentNumberDto
 import com.project.goms.domain.outing.entity.repository.OutingBlackListRepository
 import com.project.goms.domain.studentCouncil.usecase.dto.AllAccountDto
@@ -8,21 +8,24 @@ import com.project.goms.global.annotation.UseCaseWithReadOnlyTransaction
 
 @UseCaseWithReadOnlyTransaction
 class QueryAllAccountUseCase(
-    private val accountRepository: AccountRepository,
+    private val customAccountRepository: CustomAccountRepository,
     private val outingBlackListRepository: OutingBlackListRepository
 ) {
 
-    fun execute(): List<AllAccountDto> =
-        accountRepository.findAllByOrderByGradeAscClassNumAscNumberAsc()
-            .map {
-                AllAccountDto(
-                    accountIdx = it.idx,
-                    name = it.name,
-                    studentNum = StudentNumberDto(it.grade, it.classNum, it.number),
-                    profileUrl = it.profileUrl,
-                    authority = it.authority,
-                    isBlackList = outingBlackListRepository.existsById(it.idx)
-                )
-            }
+    fun execute(): List<AllAccountDto> {
+        val accountList = customAccountRepository.findAllOrderByStudentNum()
+        val outingBlackListIds = outingBlackListRepository.findAll().filterNotNull().map { it.accountIdx }
+
+        return accountList.map {
+            AllAccountDto(
+                accountIdx = it.idx,
+                name = it.name,
+                studentNum = StudentNumberDto(it.studentNum.grade, it.studentNum.classNum, it.studentNum.number),
+                profileUrl = it.profileUrl,
+                authority = it.authority,
+                isBlackList = outingBlackListIds.contains(it.idx)
+            )
+        }
+    }
 
 }
--- src/test/kotlin/com/project/goms/domain/studentCouncil/QueryAllAccountUseCaseTest.kt
+++ src/test/kotlin/com/project/goms/domain/studentCouncil/QueryAllAccountUseCaseTest.kt
@@ -2,7 +2,10 @@ package com.project.goms.domain.studentCouncil
 
 import com.project.goms.common.AnyValueObjectGenerator
 import com.project.goms.domain.account.entity.Account
-import com.project.goms.domain.account.entity.repository.AccountRepository
+import com.project.goms.domain.account.entity.Authority
+import com.project.goms.domain.account.entity.repository.CustomAccountRepository
+import com.project.goms.domain.account.usecase.dto.StudentNumberDto
+import com.project.goms.domain.outing.entity.OutingBlackList
 import com.project.goms.domain.outing.entity.repository.OutingBlackListRepository
 import com.project.goms.domain.studentCouncil.usecase.QueryAllAccountUseCase
 import com.project.goms.domain.studentCouncil.usecase.dto.AllAccountDto
@@ -10,20 +13,28 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
-import java.util.UUID
+import java.util.*
 
 class QueryAllAccountUseCaseTest: BehaviorSpec({
-    val accountRepository = mockk<AccountRepository>()
+    val customAccountRepository = mockk<CustomAccountRepository>()
     val outingBlackListRepository = mockk<OutingBlackListRepository>()
-    val queryAllAccountUseCase = QueryAllAccountUseCase(accountRepository, outingBlackListRepository)
+    val queryAllAccountUseCase = QueryAllAccountUseCase(customAccountRepository, outingBlackListRepository)
 
     Given("계정이 주어질때") {
         val accountIdx = UUID.randomUUID()
         val account = AnyValueObjectGenerator.anyValueObject<Account>("idx" to accountIdx)
-        val allAccountDto = AnyValueObjectGenerator.anyValueObject<AllAccountDto>("accountIdx" to accountIdx)
+        val allAccountDto = AllAccountDto(
+            accountIdx = accountIdx,
+            name = "",
+            studentNum = StudentNumberDto(0, 0, 0),
+            profileUrl = "",
+            authority = Authority.ROLE_STUDENT,
+            isBlackList = true
+        )
+        val outingBlackList = AnyValueObjectGenerator.anyValueObject<OutingBlackList>("accountIdx" to account.idx)
 
-        every { accountRepository.findAllByOrderByGradeAscClassNumAscNumberAsc() } returns listOf(account)
-        every { outingBlackListRepository.existsById(account.idx) } returns false
+        every { customAccountRepository.findAllOrderByStudentNum() } returns listOf(account)
+        every { outingBlackListRepository.findAll() } returns listOf(outingBlackList)
 
         When("모든 계정 조회 요청을 하면") {
             val result = queryAllAccountUseCase.execute()
