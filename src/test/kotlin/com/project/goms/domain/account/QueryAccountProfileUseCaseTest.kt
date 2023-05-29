381f20c543c1583eb714b1d36e308ad3d9038c46
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-29 23:10:30 +0900

    :ambulance: :: 프로필 isBlackList 추가

--- src/test/kotlin/com/project/goms/domain/account/QueryAccountProfileUseCaseTest.kt
+++ src/test/kotlin/com/project/goms/domain/account/QueryAccountProfileUseCaseTest.kt
@@ -6,6 +6,7 @@ import com.project.goms.domain.account.entity.Account
 import com.project.goms.domain.account.usecase.dto.ProfileDto
 import com.project.goms.domain.account.usecase.QueryAccountProfileUseCase
 import com.project.goms.domain.late.entity.repository.LateRepository
+import com.project.goms.domain.outing.entity.repository.OutingBlackListRepository
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -14,7 +15,8 @@ import io.mockk.mockk
 class QueryAccountProfileUseCaseTest: BehaviorSpec({
     val accountUtil = mockk<AccountUtil>()
     val lateRepository = mockk<LateRepository>()
-    val queryAccountProfileUseCase = QueryAccountProfileUseCase(accountUtil, lateRepository)
+    val outingBlackListRepository = mockk<OutingBlackListRepository>()
+    val queryAccountProfileUseCase = QueryAccountProfileUseCase(accountUtil, lateRepository, outingBlackListRepository)
 
     Given("계정이 주어질때") {
         val account = AnyValueObjectGenerator.anyValueObject<Account>("email" to "test@test.com")
@@ -22,6 +24,7 @@ class QueryAccountProfileUseCaseTest: BehaviorSpec({
 
         every { accountUtil.getCurrentAccount() } returns account
         every { lateRepository.countByAccountIdx(account.idx) } returns 0
+        every { outingBlackListRepository.existsById(account.idx) } returns false
 
         When("프로필 요청을 하면") {
             val result = queryAccountProfileUseCase.execute()
