72796cdc4896fefb723479ba698f0cbb557da334
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-05-30 11:15:05 +0900

    :ambulance: :: ResponseEntity 빠진거 추가

--- src/main/kotlin/com/project/goms/domain/account/presentation/AccountController.kt
+++ src/main/kotlin/com/project/goms/domain/account/presentation/AccountController.kt
@@ -3,6 +3,7 @@ package com.project.goms.domain.account.presentation
 import com.project.goms.domain.account.common.util.AccountConverter
 import com.project.goms.domain.account.presentation.data.response.ProfileResponse
 import com.project.goms.domain.account.usecase.QueryAccountProfileUseCase
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,8 +16,9 @@ class AccountController(
 ) {
 
     @GetMapping("profile")
-    fun queryProfile(): ProfileResponse =
+    fun queryProfile(): ResponseEntity<ProfileResponse> =
         queryAccountProfileUseCase.execute()
             .let { accountConverter.toResponse(it) }
+            .let { ResponseEntity.ok(it) }
 
 }
