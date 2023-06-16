4d5964f2fe1882e112943a041200ccb3d1c9035f
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-16 14:01:10 +0900

    :sparkles: :: 외출상태관리 usecase

---
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/usecase/OutingBanUseCase.kt
@@ -0,0 +1,37 @@
+package com.project.goms.domain.studentCouncil.usecase
+
+import com.project.goms.domain.studentCouncil.common.property.OutingStatusExpTimeProperties
+import com.project.goms.domain.studentCouncil.entity.OutingStatus
+import com.project.goms.domain.studentCouncil.entity.repository.OutingStatusRepository
+import com.project.goms.domain.studentCouncil.usecase.dto.OutingBanDto
+import com.project.goms.global.annotation.UseCaseWithTransaction
+import mu.KotlinLogging
+import java.util.*
+
+private val log = KotlinLogging.logger {}
+
+@UseCaseWithTransaction
+class OutingBanUseCase(
+    private val outingStatusRepository: OutingStatusRepository,
+    private val outingStatusExpTimeProperties: OutingStatusExpTimeProperties
+) {
+
+    fun execute(outingBanDto: OutingBanDto) {
+        when (outingBanDto.status) {
+            true -> {
+                val outingStatus = OutingStatus(
+                    outingStatusUUID = UUID.randomUUID(),
+                    expiredAt = outingStatusExpTimeProperties.expiredAt
+                )
+                log.info { "이번주 외출 가능" }
+                outingStatusRepository.save(outingStatus)
+            }
+
+            false -> {
+                log.info { "이번주 외출 금지 ㅅㄱ" }
+                outingStatusRepository.deleteAll()
+            }
+        }
+    }
+
+}
