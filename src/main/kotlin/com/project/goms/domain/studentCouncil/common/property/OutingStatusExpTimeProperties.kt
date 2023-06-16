cf379b6ed9cb68a979a518ce02b5dffd56e6ca8c
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-16 14:02:58 +0900

    :sparkles: :: 외출상태관리 redis 저장시간 등록 , 레포지토리 생성

---
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/common/property/OutingStatusExpTimeProperties.kt
@@ -0,0 +1,10 @@
+package com.project.goms.domain.studentCouncil.common.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("outing.status")
+class OutingStatusExpTimeProperties (
+    val expiredAt : Int
+)
