3547105aa6869cd7dccc016c153738dbd378b9f3
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-29 17:08:56 +0900

    :sparkles: :: 관리자가 외출리스트에서 외출자 삭제

---
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/usecase/DeleteOutingListUseCase.kt
@@ -0,0 +1,21 @@
+package com.project.goms.domain.studentCouncil.usecase
+
+import com.project.goms.domain.account.common.exception.AccountNotFoundException
+import com.project.goms.domain.account.entity.repository.AccountRepository
+import com.project.goms.domain.outing.entity.repository.OutingRepository
+import com.project.goms.global.annotation.UseCaseWithTransaction
+import org.springframework.data.repository.findByIdOrNull
+import java.util.*
+
+@UseCaseWithTransaction
+class DeleteOutingListUseCase(
+    private val accountRepository: AccountRepository,
+    private val outingRepository: OutingRepository
+) {
+
+    fun execute(accountIdx: UUID) {
+        val account = accountRepository.findByIdOrNull(accountIdx) ?: throw AccountNotFoundException()
+        outingRepository.deleteByAccountIdx(account.idx)
+    }
+
+}
