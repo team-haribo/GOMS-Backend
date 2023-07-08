63d51e4510752e28f34e60ce9867e641b437262b
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-08 13:11:49 +0900

    :sparkles: :: QR 생성할때 외출가능 시간,요일 관련 예외처리

---
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/common/exception/NotAvailableCreateOutingException.kt
@@ -0,0 +1,6 @@
+package com.project.goms.domain.studentCouncil.common.exception
+
+import com.project.goms.global.error.ErrorCode
+import com.project.goms.global.error.exception.GomsException
+
+class NotAvailableCreateOutingException : GomsException(ErrorCode.NOT_AVAILABLE_CREATE_OUTING)
--- src/main/kotlin/com/project/goms/global/error/ErrorCode.kt
+++ src/main/kotlin/com/project/goms/global/error/ErrorCode.kt
@@ -20,11 +20,14 @@ enum class ErrorCode(
     // OUTING
     BLACKLIST_NOT_ALLOW_OUTING("블랙리스트인 학생은 외출을 할 수 없습니다.", HttpStatus.BAD_REQUEST),
     OUTING_UUID_UNVERIFIED("검증되지 않은 외출 식별자 입니다.", HttpStatus.BAD_REQUEST),
-    NOT_AVAILABLE_OUTING_OUTING_TIME("외출가능한 시간이 아닙니다.", HttpStatus.BAD_REQUEST),
+    NOT_AVAILABLE_OUTING_TIME("외출 가능 시간이 아닙니다.", HttpStatus.BAD_REQUEST),
 
     // FEIGN
     FEIGN_BAD_REQUEST("FEIGN Bad Request", HttpStatus.BAD_REQUEST),
     FEIGN_FORBIDDEN("FEIGN Forbidden", HttpStatus.FORBIDDEN),
-    FEIGN_UNAUTHORIZED("FEIGN Unauthorized", HttpStatus.UNAUTHORIZED)
+    FEIGN_UNAUTHORIZED("FEIGN Unauthorized", HttpStatus.UNAUTHORIZED),
+
+    // STUDENT_COUNCIL
+    NOT_AVAILABLE_CREATE_OUTING("QR생성 가능 시간이 아닙니다.", HttpStatus.BAD_REQUEST)
 
 }
