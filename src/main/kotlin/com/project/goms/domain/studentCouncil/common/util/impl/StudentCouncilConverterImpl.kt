1c971d2812fa4d9ccd6ad603972230f43c46d6cd
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-09 15:30:39 +0900

    :recycle: :: StudentCouncilConverter에서 toSearchOutingResponse 삭제

--- src/main/kotlin/com/project/goms/domain/studentCouncil/common/util/impl/StudentCouncilConverterImpl.kt
+++ src/main/kotlin/com/project/goms/domain/studentCouncil/common/util/impl/StudentCouncilConverterImpl.kt
@@ -4,10 +4,8 @@ import com.project.goms.domain.account.presentation.data.response.StudentNumResp
 import com.project.goms.domain.studentCouncil.common.util.StudentCouncilConverter
 import com.project.goms.domain.studentCouncil.presentation.data.request.GrantAuthorityRequest
 import com.project.goms.domain.studentCouncil.presentation.data.response.AllAccountResponse
-import com.project.goms.domain.studentCouncil.presentation.data.response.SearchOutingResponse
 import com.project.goms.domain.studentCouncil.usecase.dto.AllAccountDto
 import com.project.goms.domain.studentCouncil.usecase.dto.GrantAuthorityDto
-import com.project.goms.domain.studentCouncil.usecase.dto.SearchOutingDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -28,15 +26,5 @@ class StudentCouncilConverterImpl: StudentCouncilConverter {
             )
         }
 
-    override fun toSearchOutingResponse(dto: List<SearchOutingDto>): List<SearchOutingResponse> =
-        dto.map {
-            SearchOutingResponse(
-                accountIdx = it.accountIdx,
-                name = it.name,
-                studentNum = StudentNumResponse(it.studentNum.grade, it.studentNum.classNum, it.studentNum.number),
-                profileUrl = it.profileUrl,
-                authority = it.authority
-            )
-        }
 
 }
