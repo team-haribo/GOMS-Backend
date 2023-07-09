3a5f24b9c035cab2773fac79e7ee9968358c99c6
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-09 15:30:18 +0900

    :recycle: :: outingConverter에 toSearchOutingResponse 추가

--- src/main/kotlin/com/project/goms/domain/outing/common/util/OutingConverter.kt
+++ src/main/kotlin/com/project/goms/domain/outing/common/util/OutingConverter.kt
@@ -2,14 +2,17 @@ package com.project.goms.domain.outing.common.util
 
 import com.project.goms.domain.account.entity.Account
 import com.project.goms.domain.outing.entity.Outing
-import com.project.goms.domain.outing.usecase.dto.OutingAccountDto
 import com.project.goms.domain.outing.presentation.data.response.OutingAccountResponse
 import com.project.goms.domain.outing.presentation.data.response.OutingCountResponse
+import com.project.goms.domain.outing.usecase.dto.OutingAccountDto
+import com.project.goms.domain.studentCouncil.presentation.data.response.SearchOutingResponse
+import com.project.goms.domain.studentCouncil.usecase.dto.SearchOutingDto
 
 interface OutingConverter {
 
     fun toEntity(account: Account): Outing
-    fun toResponse(dto: List<OutingAccountDto>): List<OutingAccountResponse>
+    fun toOutingAccountResponse(dto: List<OutingAccountDto>): List<OutingAccountResponse>
     fun toResponse(outingCount: Long): OutingCountResponse
+    fun toSearchOutingResponse(dto: List<SearchOutingDto>): List<SearchOutingResponse>
 
 }
--- src/main/kotlin/com/project/goms/domain/outing/common/util/impl/OutingConverterImpl.kt
+++ src/main/kotlin/com/project/goms/domain/outing/common/util/impl/OutingConverterImpl.kt
@@ -2,11 +2,13 @@ package com.project.goms.domain.outing.common.util.impl
 
 import com.project.goms.domain.account.entity.Account
 import com.project.goms.domain.account.presentation.data.response.StudentNumResponse
-import com.project.goms.domain.outing.entity.Outing
 import com.project.goms.domain.outing.common.util.OutingConverter
-import com.project.goms.domain.outing.usecase.dto.OutingAccountDto
+import com.project.goms.domain.outing.entity.Outing
 import com.project.goms.domain.outing.presentation.data.response.OutingAccountResponse
 import com.project.goms.domain.outing.presentation.data.response.OutingCountResponse
+import com.project.goms.domain.outing.usecase.dto.OutingAccountDto
+import com.project.goms.domain.studentCouncil.presentation.data.response.SearchOutingResponse
+import com.project.goms.domain.studentCouncil.usecase.dto.SearchOutingDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,7 +17,7 @@ class OutingConverterImpl : OutingConverter {
     override fun toEntity(account: Account): Outing =
         Outing(idx = -1, account = account)
 
-    override fun toResponse(dto: List<OutingAccountDto>): List<OutingAccountResponse> =
+    override fun toOutingAccountResponse(dto: List<OutingAccountDto>): List<OutingAccountResponse> =
         dto.map {
             OutingAccountResponse(
                 accountIdx = it.accountIdx,
@@ -28,5 +30,16 @@ class OutingConverterImpl : OutingConverter {
 
     override fun toResponse(outingCount: Long): OutingCountResponse =
         OutingCountResponse(outingCount = outingCount)
+
+    override fun toSearchOutingResponse(dto: List<SearchOutingDto>): List<SearchOutingResponse> =
+        dto.map {
+            SearchOutingResponse(
+                accountIdx = it.accountIdx,
+                name = it.name,
+                studentNum = StudentNumResponse(it.studentNum.grade, it.studentNum.classNum, it.studentNum.number),
+                profileUrl = it.profileUrl,
+                authority = it.authority
+            )
+        }
 
 }
