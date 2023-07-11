5fd172698cda950947735b801f16d2e8e0893046
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-11 13:59:10 +0900

    :sparkles: :: OutingConverter 추가

--- src/main/kotlin/com/project/goms/domain/outing/common/util/OutingConverter.kt
+++ src/main/kotlin/com/project/goms/domain/outing/common/util/OutingConverter.kt
@@ -3,6 +3,7 @@ package com.project.goms.domain.outing.common.util
 import com.project.goms.domain.account.entity.Account
 import com.project.goms.domain.outing.entity.Outing
 import com.project.goms.domain.outing.presentation.data.response.OutingAccountResponse
+import com.project.goms.domain.outing.presentation.data.response.OutingAvailableResponse
 import com.project.goms.domain.outing.presentation.data.response.OutingCountResponse
 import com.project.goms.domain.outing.usecase.dto.OutingAccountDto
 
@@ -11,5 +12,6 @@ interface OutingConverter {
     fun toEntity(account: Account): Outing
     fun toResponse(dto: List<OutingAccountDto>): List<OutingAccountResponse>
     fun toResponse(outingCount: Long): OutingCountResponse
+    fun toResponse(isOutingAvailable: Boolean): OutingAvailableResponse
 
 }
