72e5464f2a21c690d752d95da949775874f9fd39
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-23 21:20:24 +0900

    :sparkles: :: 현재 외출 가능한 시간인지 구분하는 기능 테스트 코드 작성

--- src/test/kotlin/com/project/goms/domain/outing/ValidateOutingTimeUseCaseTest.kt
+++ src/test/kotlin/com/project/goms/domain/outing/ValidateOutingTimeUseCaseTest.kt
@@ -9,8 +9,18 @@ import io.mockk.unmockkStatic
 import java.time.LocalDate
 import java.time.LocalTime
 
-class ValidateOutingTimeUseCaseTest : BehaviorSpec({
+class ValidateOutingTimeUseCaseTest: BehaviorSpec({
     val outingUseCase = ValidateOutingTimeUseCase()
+
+    fun beforeTest() {
+        mockkStatic(LocalDate::class)
+        mockkStatic(LocalTime::class)
+    }
+
+    fun afterTest() {
+        unmockkStatic(LocalDate::class)
+        unmockkStatic(LocalTime::class)
+    }
 
     Given("ValidateOutingTimeUseCase를 실행했을때 ") {
 
@@ -18,20 +28,19 @@ class ValidateOutingTimeUseCaseTest : BehaviorSpec({
             val wednesday = LocalDate.of(2023, 7, 26)
             val startTime = LocalTime.of(18, 55)
 
-            mockkStatic(LocalDate::class)
-            mockkStatic(LocalTime::class)
+            beforeTest()
 
             every { LocalDate.now() } returns wednesday
             every { LocalTime.now() } returns startTime
 
             val result = outingUseCase.execute()
 
-            Then("true를 반환해야 합니다") {
+            Then("true를 반환해야 합니다.") {
                 result shouldBe true
             }
 
-            unmockkStatic(LocalDate::class)
-            unmockkStatic(LocalTime::class)
+            afterTest()
+
         }
 
         When("수요일이 아니거나 18:50에서 19:25 사이가 아닐 때") {
@@ -39,20 +48,18 @@ class ValidateOutingTimeUseCaseTest : BehaviorSpec({
             val timeBeforeStart = LocalTime.of(18, 45)
             val timeAfterEnd = LocalTime.of(19, 30)
 
-            mockkStatic(LocalDate::class)
-            mockkStatic(LocalTime::class)
+            beforeTest()
 
             every { LocalDate.now() } returns tuesday
             every { LocalTime.now() } returns timeBeforeStart andThen timeAfterEnd
 
             val result = outingUseCase.execute()
 
-            Then("false를 반환해야 합니다") {
+            Then("false를 반환해야 합니다.") {
                 result shouldBe false
             }
 
-            unmockkStatic(LocalDate::class)
-            unmockkStatic(LocalTime::class)
+            afterTest()
         }
     }
 })
