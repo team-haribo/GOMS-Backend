7346c315537b9a5508e4ce143513170b01b156f3
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-02 22:23:26 +0900

    :fire: :: 디스코드 구현체, feign 삭제

--- src/main/kotlin/com/project/goms/domain/outing/scheduler/OutingScheduler.kt
+++ src/main/kotlin/com/project/goms/domain/outing/scheduler/OutingScheduler.kt
@@ -2,19 +2,14 @@ package com.project.goms.domain.outing.scheduler
 
 import com.project.goms.domain.late.usecase.SaveLateAccountUseCase
 import com.project.goms.domain.outing.usecase.DeleteOutingStudentsUseCase
-import com.project.goms.domain.outing.usecase.ReminderOutingUseCase
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class OutingScheduler(
-    private val reminderOutingUseCase: ReminderOutingUseCase,
     private val saveRateStudentUseCase: SaveLateAccountUseCase,
     private val deleteOutingStudentsUseCase: DeleteOutingStudentsUseCase,
 ) {
-
-    @Scheduled(cron = "0 20 18 ? * 3") // 매주 수요일 6시 20분에 외출 여부 디스코드를 보낸다.
-    fun sendOutingMessage() = reminderOutingUseCase.execute()
 
     @Scheduled(cron = "0 25 19 ? * 3") // 매주 수요일 7시 25분에 지각자를 저장한다.
     fun checkRateStudent() = saveRateStudentUseCase.execute()
