156236973d37e73f526c5a80f86e87d16432befc
KimGyeongsuuu <sk62628504@gmail.com>
2023-11-17 08:18:17 +0900

    :recycle:  :: outingBlackList 에서 지각자 수 가져오기

--- src/main/kotlin/com/project/goms/domain/notification/usecase/SendNotificationUseCase.kt
+++ src/main/kotlin/com/project/goms/domain/notification/usecase/SendNotificationUseCase.kt
@@ -2,57 +2,50 @@ package com.project.goms.domain.notification.usecase
 
 import com.project.goms.domain.notification.common.exception.DeviceTokenNotFoundException
 import com.project.goms.domain.notification.spi.NotificationPort
-import com.project.goms.domain.late.entity.repository.LateRepository
 import com.project.goms.domain.notification.entity.*
 import com.project.goms.domain.notification.entity.repository.DeviceTokenRepository
+import com.project.goms.domain.outing.entity.repository.OutingBlackListRepository
 import com.project.goms.domain.outing.entity.repository.OutingRepository
 import com.project.goms.global.annotation.UseCaseWithReadOnlyTransaction
 import org.springframework.data.repository.findByIdOrNull
-import java.time.LocalDate
 import java.util.*
 
 @UseCaseWithReadOnlyTransaction
 class SendNotificationUseCase(
-    private val lateRepository: LateRepository,
+    private val outingBlackListRepository: OutingBlackListRepository,
     private val notificationPort: NotificationPort,
     private val deviceTokenRepository: DeviceTokenRepository,
     private val outingRepository: OutingRepository,
 ) {
 
     fun execute(notificationType: NotificationType) {
-        val oneWeekAgo = LocalDate.now().minusWeeks(1)
 
         when (notificationType) {
             // 외출 전 지난주 지각자 수에 따라서 외출 알림 발송
             NotificationType.BEFORE_OUTING -> {
-                runCatching {
-                    when (lateRepository.lateCountOntWeekAgo(oneWeekAgo) < 3) {
-                        true -> {
-                            notificationPort.sendNotification(
-                                deviceTokens = deviceTokenRepository.findAll().map { it.token },
-                                notification = Notification(
-                                    title = Topic.BEFORE_OUTING.title,
-                                    content = Topic.BEFORE_OUTING.content,
-                                    writer = Writer.GOMS
-                                )
+                when (outingBlackListRepository.count() < 3) {
+                    true -> {
+                        notificationPort.sendNotification(
+                            deviceTokens = deviceTokenRepository.findAll().map { it.token },
+                            notification = Notification(
+                                title = Topic.BEFORE_OUTING.title,
+                                content = Topic.BEFORE_OUTING.content,
+                                writer = Writer.GOMS
                             )
-                        }
-
-                        false -> {
-                            notificationPort.sendNotification(
-                                deviceTokens = deviceTokenRepository.findAll().map { it.token },
-                                notification = Notification(
-                                    title = Topic.GROUNDED.title,
-                                    content = Topic.GROUNDED.content,
-                                    writer = Writer.GOMS
-                                )
-                            )
-                        }
+                        )
                     }
-                }.onFailure {
-                    it.printStackTrace()
-                }
 
+                    false -> {
+                        notificationPort.sendNotification(
+                            deviceTokens = deviceTokenRepository.findAll().map { it.token },
+                            notification = Notification(
+                                title = Topic.GROUNDED.title,
+                                content = Topic.GROUNDED.content,
+                                writer = Writer.GOMS
+                            )
+                        )
+                    }
+                }
             }
 
             // 외출 5분 전 아직 복귀하지 않은 학생들에게 알림 발송
